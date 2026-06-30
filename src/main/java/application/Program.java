65402419342642c680ae4d9fa114df4335a13b9f
Vinicius <viniciusnogueira2507@gmail.com>
2026-06-30 15:27:20 -0300

    adiciona Seller e Department

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department department = new Department(1, "books");
+
+        Seller seller = new Seller(1, "John" , "john@gmail.com" , new Date() , 3000.00 ,  department );
+
+        System.out.println(department);
+        System.out.println(seller);
+
+
+
+    }
+}
---
+++ src/main/java/model/entities/Department.java
@@ -0,0 +1,50 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private Integer id;
+    private String name;
+
+    public Department( Integer id , String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
